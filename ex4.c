05065ede5f6324bfa52a86d428b37917c88734d4
Anna Letícia Silva <annaleticiabarbosadasilva@gmail.com>
2025-05-03 16:59:01 -0300

    Create ex4.c

---
+++ ex4.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "igualdade.h"
+
+
+int main (){
+    int num1;
+    int num2;
+    
+    printf("Digite o primeiro número: ");
+    scanf("%d", &num1);
+    
+    printf("Digite o segundo número: ");
+    scanf("%d", &num2);
+    
+    printf("\nIguais: %d ", igual(num1,num2));
+    printf("\nMaior: %d ", maior(num1,num2));
+    printf("\nMenor: %d \n", menor(num1,num2));
+    
+    return 0;
+}
